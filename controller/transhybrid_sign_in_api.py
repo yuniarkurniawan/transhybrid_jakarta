48805dc19c908654c82d7d629a4f3854a6c88f90
Yuniar <r.yuniar@wgs.co.id>
2018-09-15 09:40:40 +0700

    1. user login check

--- controller/transhybrid_sign_in_api.py
+++ controller/transhybrid_sign_in_api.py
@@ -39,8 +39,85 @@ class TranshybridSignInApi(http.Controller):
 		return kondisi
 
 
+
+
 	@http.route("/signin/",auth='none',csrf=False, type='json')
 	def signIn(self,**values):
+
+		resUsersModel = request.env['res.users']
+
+		body = request.jsonrequest
+		username = body['username']
+		password = body['password']
+
+		if(self.check_empty_input_user(username)):
+			return {
+					'code': 404,
+					'message': 'Username Can Not Be Empty'
+				}
+
+		if(self.check_empty_input_user(password)):
+			return {
+					'code': 404,
+					'message': 'Password Can Not Be Empty'
+				}
+
+
+		headerData = request.httprequest.headers		
+		headerToken =  headerData.get('Apptoken')
+
+
+		dbname = ''
+		if request.session.db:
+			dbname = request.session.db
+			uid = request.session.uid
+		elif dbname is None:
+			dbname = db_monodb()
+
+
+		uid = resUsersModel.authenticate(dbname, username, password, None)
+		uid = resUsersModel.sudo().browse(uid)
+
+
+		if(uid):
+
+			
+			dataUser = resUsersModel.sudo().search([('id','=',uid.id)],limit=1,order="id desc")
+			if(dataUser):
+
+				listAvatar = []
+				listAvatar.append(str("data:image/png;base64,"))
+				listAvatar.append(str(uid.image))
+				outAvatar = ''.join(listAvatar)
+				
+				return{
+					"username": username,
+					"name": uid.name,
+					"avatar": outAvatar,
+					"code" : 200,
+					"message" : "OK",
+				}
+				
+			else:
+
+				return {
+						  "code": 404,
+						  "message": "User Is Not Found"
+						}
+			
+
+		else:
+
+			return {
+				  "code": 404,
+				  "message": "User And Password Is Not Found"
+				}
+
+
+
+
+	@http.route("/signin_original/",auth='none',csrf=False, type='json')
+	def signInOriginal(self,**values):
 
 		resUsersModel = request.env['res.users']
 
