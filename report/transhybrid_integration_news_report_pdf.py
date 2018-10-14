5b1fb1bc6b1b362385e60f92197997ecf639245d
Yuniar <r.yuniar@wgs.co.id>
2018-10-15 05:51:26 +0700

    1. Perubahan terbaru

---
+++ report/transhybrid_integration_news_report_pdf.py
@@ -0,0 +1,32 @@
+from openerp import api,models
+
+class TranshybridIntegrationNewsReportPDF(models.AbstractModel):
+
+	_name = "report.transhybrid_jakarta.integration_news_report_pdf"
+
+
+	def get_all_data(self, docs):
+
+		listingData = []
+		for object in docs:
+			listingData.append(object.name)
+
+		listing = list(set(listingData))
+
+		return listing
+
+	    
+	@api.multi
+	def render_html(self,docids,data=None):
+
+		report_obj = self.env['report']
+		report = report_obj._get_report_from_name('transhybrid_jakarta.integration_news_report_pdf')
+
+		docargs = {
+
+			'doc_ids' : docids,
+			'doc_model':report.model,
+			'docs':self,
+		}
+
+		return report_obj.render("transhybrid_jakarta.integration_news_report_pdf")
